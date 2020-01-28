fc8e3244dd912993042ac8616ae10cf7366d1fbf
nicoleyimessier <nicoleyimessier@gmail.com>
2020-01-28 15:36:47 -0500

    updated main

--- main.py
+++ main.py
@@ -9,57 +9,74 @@ import os
 
 # Helper for looking up cells by column name
 def getCell(colName, rowIndex, values):
-    return values[rowIndex][values[0].index(colName)]
+	return values[rowIndex][values[0].index(colName)]
+
+
+def parseSheet(values, data, key):
+	data[key] = []; 
+	for rowIndex in range(1, len(values)):
+		assetReady = (getCell("Asset Ready?", rowIndex, values))
+
+		if assetReady == "TRUE": 
+			imgObj = {}
+			imgObj["file_name"] = (getCell("File Name", rowIndex, values));
+			imgObj["image_type"] = getCell("Image Type", rowIndex, values);
+			imgObj["collage_category"] = []
+			collage_category = getCell("Collage Category", rowIndex, values);
+			collage_categories = collage_category.split(",")
+			imgObj["collage_category"] = collage_categories
+			print (key + ": " + imgObj["file_name"])
+
+			# Add this assets to the list
+			data[key].append(imgObj)
+
 
 def downloadData(keyfile, location):
-    print "--- BEGIN RETRIEVING DATA ---"
+	print "--- BEGIN RETRIEVING DATA ---"
 
-    # Set up JSON
-    data = { "images": []}
+	# Set up JSON
+	data = {}
  
 
-    # Download from Google Sheets
-    # https://github.com/burnash/gspread
-    # https://github.com/googleapis/oauth2client
-    gsheet = GoogleAPI.GSheets(keyfile)
-    
+	# Download from Google Sheets
+	# https://github.com/burnash/gspread
+	# https://github.com/googleapis/oauth2client
+	gsheet = GoogleAPI.GSheets(keyfile)
+	
 
-    # First, download Park Life tab by feeding the ("spreadsheet name", "spreadsheet tab name")
-    objects = gsheet.download("ParkModern_Collage_Image_Tracker", "Park Life") 
-    values = objects.get_all_values()
-    
-    for rowIndex in range(1, len(values)):
-        assetReady = (getCell("Asset Ready?", rowIndex, values))
+	# First, download Park Life tab by feeding the ("spreadsheet name", "spreadsheet tab name")
+	objects = gsheet.download("ParkModern_Collage_Image_Tracker", "Park Life") 
+	_values = objects.get_all_values()
+	parseSheet(_values, data, "park_life")
 
-        if assetReady == "TRUE": 
-            imgObj = {}
-            imgObj["file_name"] = (getCell("File Name", rowIndex, values));
-            imgObj["image_type"] = getCell("Image Type", rowIndex, values);
-            imgObj["collage_category"] = []
-            collage_category = getCell("Collage Category", rowIndex, values);
-            collage_categories = collage_category.split(",")
-            imgObj["collage_category"] = collage_categories
-            print imgObj["file_name"]
+	objects = gsheet.download("ParkModern_Collage_Image_Tracker", "Regeneration") 
+	_values = objects.get_all_values()
+	parseSheet(_values, data, "regeneration")
+	
+	objects = gsheet.download("ParkModern_Collage_Image_Tracker", "Fenton Whelan") 
+	_values = objects.get_all_values()
+	parseSheet(_values, data, "fenton")
 
-            # Add this assets to the list
-            data["images"].append(imgObj)
+	objects = gsheet.download("ParkModern_Collage_Image_Tracker", "Building Amenities") 
+	_values = objects.get_all_values()
+	parseSheet(_values, data, "amenities")
 
-    # Write to file
-    with io.open(location, 'w+', encoding='utf8') as json_file:
-        data = json.dumps(data, indent=4, ensure_ascii=False)
-        json_file.write(unicode(data))
+	# Write to file
+	with io.open(location, 'w+', encoding='utf8') as json_file:
+		data = json.dumps(data, indent=4, ensure_ascii=False)
+		json_file.write(unicode(data))
 
-    print "--- FINISHED RETRIEVING DATA ---"
+	print "--- FINISHED RETRIEVING DATA ---"
 
 if __name__ == "__main__":
 
-    print(sys.argv)
-    if len(sys.argv) != 3:
-        print "usage: <path/to/api/keyfile> <path/to/downloaded/json>"
-        exit()
-    elif os.path.exists(sys.argv[1]):
-        downloadData(sys.argv[1], sys.argv[2])
-    else:
-        print "ERROR: keyfile arg is not valid path!"
-        exit()
+	print(sys.argv)
+	if len(sys.argv) != 3:
+		print "usage: <path/to/api/keyfile> <path/to/downloaded/json>"
+		exit()
+	elif os.path.exists(sys.argv[1]):
+		downloadData(sys.argv[1], sys.argv[2])
+	else:
+		print "ERROR: keyfile arg is not valid path!"
+		exit()
 
--- templates.py
+++ templates.py
@@ -15,7 +15,7 @@ def downloadData(keyfile, location):
     print "--- BEGIN RETRIEVING DATA ---"
 
     # Set up JSON
-    data = { "images": []}
+    data = { "collages": []}
  
 
     # Download from Google Sheets
@@ -32,24 +32,63 @@ def downloadData(keyfile, location):
         imgObj = {}
         imgObj["uid"] = (getCell("UID", rowIndex, values));
         imgObj["category"] = getCell("Category", rowIndex, values);
-        imgObj["num_images"] = getCell("# of Images", rowIndex, values);
+        imgObj["num_images"] = int(getCell("# of Images", rowIndex, values));
+        imgObj["boxes"] = []; 
         
         #1
-        imgObj["box1_width"] = getCell("Box 1 Width", rowIndex, values);
-        imgObj["box1_height"] = getCell("Box 1 Height", rowIndex, values);
-        imgObj["box1_xpos"] = (getCell("Box 1 X Pos", rowIndex, values));
-        imgObj["box1_ypos"] = (getCell("Box 1 Y Pos", rowIndex, values)); 
+        box = {}
+        box["width"] = int(getCell("Box 1 Width", rowIndex, values));
+        box["height"] = int(getCell("Box 1 Height", rowIndex, values));
+        box["xpos"] = int(getCell("Box 1 X Pos", rowIndex, values));
+        box["ypos"] = int(getCell("Box 1 Y Pos", rowIndex, values)); 
+        imgObj["boxes"].append(box);
 
         #2
+        box = {}
+        box["width"] = int(getCell("Box 2 Width", rowIndex, values));
+        box["height"] = int(getCell("Box 2 Height", rowIndex, values));
+        box["xpos"] = int(getCell("Box 2 X Pos", rowIndex, values));
+        box["ypos"] = int(getCell("Box 2 Y Pos", rowIndex, values)); 
+        imgObj["boxes"].append(box);
+
         #3
+        box = {}
+        box["width"] = int(getCell("Box 3 Width", rowIndex, values));
+        box["height"] = int(getCell("Box 3 Height", rowIndex, values));
+        box["xpos"] = int(getCell("Box 3 X Pos", rowIndex, values));
+        box["ypos"] = int(getCell("Box 3 Y Pos", rowIndex, values)); 
+        imgObj["boxes"].append(box);
+
         #4
-        #5
-        #6
+        box = {}
+        box["width"] = int(getCell("Box 4 Width", rowIndex, values));
+        box["height"] = int(getCell("Box 4 Height", rowIndex, values));
+        box["xpos"] = int(getCell("Box 4 X Pos", rowIndex, values));
+        box["ypos"] = int(getCell("Box 4 Y Pos", rowIndex, values)); 
+        imgObj["boxes"].append(box);
+
+        if imgObj["num_images"] >= 5:
+            #5
+            box = {}
+            box["width"] = int(getCell("Box 5 Width", rowIndex, values));
+            box["height"] = int(getCell("Box 5 Height", rowIndex, values));
+            box["xpos"] = int(getCell("Box 5 X Pos", rowIndex, values));
+            box["ypos"] = int(getCell("Box 5 Y Pos", rowIndex, values));
+            imgObj["boxes"].append(box); 
+
+        if imgObj["num_images"] == 6:
+            #6
+            box = {}
+            box["width"] = int(getCell("Box 6 Width", rowIndex, values));
+            box["height"] = int(getCell("Box 6 Height", rowIndex, values));
+            box["xpos"] = int(getCell("Box 6 X Pos", rowIndex, values));
+            box["ypos"] = int(getCell("Box 6 Y Pos", rowIndex, values));
+            imgObj["boxes"].append(box); 
 
         print imgObj["uid"]
 
         # Add this assets to the list
-        data["images"].append(imgObj)
+        data["collages"].append(imgObj)
 
     # Write to file
     with io.open(location, 'w+', encoding='utf8') as json_file:
